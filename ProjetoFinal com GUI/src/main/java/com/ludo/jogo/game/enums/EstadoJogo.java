7c198091d40466d4a9ef39a7a60e0f0a6d8b1c33
tenorio-sousa <tenorio.hej@gmail.com>
2025-11-30 22:33:19 -0300

    atualização

--- ProjetoFinal com GUI/src/main/java/com/ludo/jogo/game/enums/EstadoJogo.java
+++
@@ -1,8 +0,0 @@
-package com.ludo.jogo.game.enums;
-
-/** Representa o estado global da partida */
-public enum EstadoJogo {
-    EM_JOGO,
-    PAUSADO,
-    ENCERRADO;
-}
--- ProjetoFinal com GUI/src/main/java/com/ludo/jogo/game/enums/EstadoPeao.java
+++
@@ -1,8 +0,0 @@
-package com.ludo.jogo.game.enums;
-
-/** Indica a situacao atual de um peao no tabuleiro */
-public enum EstadoPeao {
-    BASE,
-    JOGANDO,
-    FINALIZADO
-}
